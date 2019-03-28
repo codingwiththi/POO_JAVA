9023cea5fd4657d0838cada628d3424e3cab7833
20162BSI0317 <20162BSI0317@SR6742.cefetes.br>
2019-03-28 08:28:34 -0300

    primeiro commit jogo

---
+++ src/jogo/Jogo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jogo;
+
+/**
+ *
+ * @author 20162BSI0317
+ */
+public class Jogo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        Questao novaQuestao = new Questao ();
+        novaQuestao.criarPergunta();
+    }
+    
+}
