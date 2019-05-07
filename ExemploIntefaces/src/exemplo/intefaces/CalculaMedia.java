2f7e64c84b95e5943c70eea9fdb258ffd92c1226
thiagomr8 <thiagom.r@hotmail.com>
2019-05-07 13:20:08 -0300

    Alterado o resultado da mediana e da media

--- ExemploIntefaces/src/exemplo/intefaces/CalculaMedia.java
+++ ExemploIntefaces/src/exemplo/intefaces/CalculaMedia.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public class CalculaMedia implements ICalcula{        
     @Override
-    public double calcula(List<Integer> lista){        
-        int total=0;
+    public double calcula(List<Integer> lista){      
+        double total=0;
         for(Integer item : lista){
             total+= item;
         }        
--- ExemploIntefaces/src/exemplo/intefaces/CalculaMediana.java
+++ ExemploIntefaces/src/exemplo/intefaces/CalculaMediana.java
@@ -7,12 +7,14 @@ public class CalculaMediana implements ICalcula{
      @Override
      public double calcula(List<Integer> lista){             
          double mediana = 0;
-         if(lista.size() % 2 == 0){//par         
-             mediana = (lista.get((lista.size()/2)-1) + lista.get((lista.size()/2)))/2;             
+         double soma;
+         if(lista.size() % 2 == 0){//par
+             soma = (lista.get((lista.size()/2)-1) + lista.get((lista.size()/2)));
+             mediana = soma/2;
          }else{//impar
             int meio = (lista.size()/2);
             mediana = lista.get(meio);
          }
-        return mediana;    
+        return mediana;
     }
 }
